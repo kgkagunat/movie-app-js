9d4b6710167c6d518ec8f0d0e49f20a9bc079484
kgkagunat <kgkagunat@gmail.com>
2023-10-17 14:21:25 -1000

    added init function

--- js/script.js
+++ js/script.js
@@ -3,3 +3,41 @@ const globalWindow = {
     currentPage: window.location.pathname,
 };
 console.log(globalWindow.currentPage);
+
+// Init
+function init() {
+    switch (globalWindow.currentPage) {
+        // Home Page
+        case '/':
+            console.log(
+                `Current Page: ${globalWindow.currentPage} (Home Page)`
+            );
+            break;
+        // TV Shows Page
+        case '/shows.html':
+            console.log(
+                `Current Page: ${globalWindow.currentPage} (TV Shows Page)`
+            );
+            break;
+        // Movies Details Page
+        case '/movie-details.html':
+            console.log(
+                `Current Page: ${globalWindow.currentPage} (Movie Details Page)`
+            );
+            break;
+        // TV Details Page
+        case '/tv-details.html':
+            console.log(
+                `Current Page: ${globalWindow.currentPage} (TV Details Page)`
+            );
+            break;
+        // Search Page
+        case '/search.html':
+            console.log(
+                `Current Page: ${globalWindow.currentPage} (Search Page)`
+            );
+            break;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', init);
